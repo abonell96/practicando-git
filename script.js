907ee4497512767aafe63af59c3a2c53590d847d
Alfredo Bonell <61245373+abonell96@users.noreply.github.com>
2022-05-05 10:03:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function consumirServicio(contenedor, url, parametros, niveles, callback){
 }
 
 consumirServicio('#contenedor_principal', 'https://8bc7-177-240-51-61.ngrok.io/administrador-contenido/', JSON.stringify({Nombre: 'Alfredo', Apellido: 'Bonell'}), '../../', 'callback');
+
+//Desarrollador uno
+
+
+
+//Desarrollador dos
+
