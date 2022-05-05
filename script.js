6b7d10b9e0292b8b6fb3c69cfef1befe6aeca1a6
Alfredo Bonell <61245373+abonell96@users.noreply.github.com>
2022-05-05 09:51:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function consumirServicio(contenedor, url, parametros, niveles, callback){
-    console.log(`Contenedor: ${contenedor},\nUrl: ${url},\nParametros: ${parametros},\nNiveles: ${niveles},\nCallback: ${callback}`);
+function consumirServicio(contenedor, url, parametros, niveles, callback, edad){
+    console.log(`Contenedor: ${contenedor},\nUrl: ${url},\nParametros: ${parametros},\nNiveles: ${niveles},\nCallback: ${callback},\nEdad: ${edad}`);
 }
 
-consumirServicio('#contenedor_principal', 'https://8bc7-177-240-51-61.ngrok.io/administrador-contenido/', JSON.stringify({Nombre: 'Alfredo', Apellido: 'Bonell'}), '../../', 'callback');
+consumirServicio('#contenedor_principal', 'https://8bc7-177-240-51-61.ngrok.io/administrador-contenido/', JSON.stringify({Nombre: 'Alfredo', Apellido: 'Bonell'}), '../../', 'callback', 26);
