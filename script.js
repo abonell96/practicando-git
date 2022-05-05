ffcd64bf0babfd2d7c07a7634593fc7d44f75c55
Alfredo Bonell <61245373+abonell96@users.noreply.github.com>
2022-05-05 12:37:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log('Agregando un cambio nuevo');
 
 //Desarrollador uno
 
+function resta(num1, num2){
+  let resultado = num1 + num2;
+  console.log(`El resultado es: ${resultado}`);
+}
 
 //Desarrollador dos
 
