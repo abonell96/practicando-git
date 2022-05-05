8ac8f40ae118b7934c2379512626f7efaf72283c
Alfredo Bonell <61245373+abonell96@users.noreply.github.com>
2022-05-05 09:49:00 -0600

    Primera prueba.

---
+++ script.js
@@ -0,0 +1,5 @@
+function consumirServicio(contenedor, url, parametros, niveles, callback){
+    console.log(`Contenedor: ${contenedor},\nUrl: ${url},\nParametros: ${parametros},\nNiveles: ${niveles},\nCallback: ${callback}`);
+}
+
+consumirServicio('#contenedor_principal', 'https://8bc7-177-240-51-61.ngrok.io/administrador-contenido/', JSON.stringify({Nombre: 'Alfredo', Apellido: 'Bonell'}), '../../', 'callback');
